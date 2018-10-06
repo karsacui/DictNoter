09251c2420fd2cdc7924d6e9d3ee54e4842d200b
karsacui <karsacui@outlook.com>
2018-10-06 12:18:18 +0800

    增加功能

--- note.sh
+++ note.sh
@@ -3,10 +3,13 @@ IFS_OLD=$IFS
 IFS=$'\n'
 conv(){
 for i in $*; do
-    grep -i 00000"$i"lllll dictyh1.html >>./output/${name/"txt"/"html"}
+grep -i 00000"$i"lllll dictyh1.html >>./output/${name/"txt"/"html"}
 done
-    sed --quiet --in-place 's#.*\(</font><hr color=blue.*\)#\1#p' ./output/${name/"txt"/"html"}
-    sed -i '1s/^\(\xef\xbb\xbf\)\?/\xef\xbb\xbf/' ./output/${name/"txt"/"html"}  
+sed --quiet --in-place 's#.*\(</font><hr color=blue.*\)#\1#p' ./output/${name/"txt"/"html"}
+sed -i "1i\<font color=blue size=+4><b>${name/".txt"/}</b></font>" ./output/${name/"txt"/"html"}
+sed -i 's!^</font><hr color=blue noshade>!<hr style=" height: 4px; border: none; background-color: blue">!g' ./output/${name/"txt"/"html"}
+sed -i 's!<hr color=blue noshade>!<hr style=" height: 1px; border: none; background-color: blue">!g' ./output/${name/"txt"/"html"}
+sed -i '1s/^\(\xef\xbb\xbf\)\?/\xef\xbb\xbf/' ./output/${name/"txt"/"html"}  
 }
 fun(){
 for i in $*; do
@@ -15,5 +18,6 @@ conv $(less ./input/$i)
 done
 }
 files=$(ls ./input/)
+rm -rf ./output/*
 fun $files
 IFS=$IFS_OLD
